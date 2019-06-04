e8f5da12489741ff77598d15566844fa194cce77
Anthony Damico <anthony.damico721@gmail.com>
2019-06-04 13:33:48 -0400

    finished lecture 42

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Anthony', age: 28 },
+      { name: 'Britt', age: 32 },
+      { name: 'Paul', age: 58 }
+    ]
+  };
+
+  handleSwitchName = () => {
+    this.setState({
+      persons: [
+        { name: 'Tony', age: 28 },
+        { name: 'Britt', age: 32 },
+        { name: 'Paul', age: 58 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Anthony" age="28" >My Hobbies: Sports</Person>
-        <Person name="Britt" age="32" />
-        <Person name="Paul" age="58" />
+        <button onClick={this.handleSwitchName}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >My Hobbies: Sports</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
