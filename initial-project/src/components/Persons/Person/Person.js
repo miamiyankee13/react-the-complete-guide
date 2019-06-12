bed6219cf02c97e09b82d0635b7f0d807761bbf5
Anthony Damico <anthony.damico721@gmail.com>
2019-06-12 11:49:50 -0400

    completed lecture 107

--- initial-project/src/components/Persons/Person/Person.js
+++ initial-project/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from './Person.module.css';
 
 class Person extends Component {
@@ -6,7 +7,9 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
         return (
             <div className={styles.person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!
+                </p>
                 <p>{this.props.children}</p>
                 <input 
                     type="text" 
@@ -17,4 +20,12 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    change: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number
+};
+
 export default Person;
