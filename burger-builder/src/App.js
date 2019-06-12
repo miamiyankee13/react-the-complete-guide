1cf07bfe660b18d32a6081fdb00ac9015f2f9b2d
Anthony Damico <anthony.damico721@gmail.com>
2019-06-12 15:32:22 -0400

    completed lecture 151

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
     return (
         <div>
-            <p>Deh</p>
+            <Layout>
+                <p>Test</p>
+            </Layout>
         </div>
     );
 }
