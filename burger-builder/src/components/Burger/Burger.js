081324db067a162abac98b14958d50077f5370f3
Anthony Damico <anthony.damico721@gmail.com>
2019-06-13 19:07:13 -0400

    completed lecture 157

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './Burger.module.css';
 
 const Burger = props => {
+    let transformedIngredients = [];
+    for (let ingredient in props.ingredients) {
+        for (let i = 0; i < props.ingredients[ingredient]; i++) {
+            transformedIngredients.push(<BurgerIngredient key={ingredient + i} type={ingredient} />)
+        }
+    }
+
     return (
         <div className={styles.burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients.length ? transformedIngredients : <p>Please add some ingredients!</p>}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,19 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            lettuce: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 1,
+        }
+    };
+
     render() {
         return (
             <Fragment>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Fragment>
         );
