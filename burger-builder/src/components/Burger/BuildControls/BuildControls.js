624e0fc5c8cb47257bdcf96e23e495571cefa296
Anthony Damico <anthony.damico721@gmail.com>
2019-06-14 11:34:58 -0400

    completed lecture 160

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
 import styles from './BuildControls.module.css';
 
-const BuildControls = props => (
-    <div>
+const controls = [
+    { label: 'Lettuce', type: 'lettuce' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
 
+const BuildControls = props => (
+    <div className={styles.buildControls}>
+        {controls.map(control => (
+            <BuildControl 
+                key={control.label} 
+                label={control.label}
+                added={() => props.ingredientAdded(control.type)}
+            />
+        ))}
     </div>
 );
 
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    lettuce: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+} 
 
 class BurgerBuilder extends Component {
     state = {
@@ -8,14 +16,44 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0,
-        }
+        },
+        totalPrice: 4
     };
+
+    //create new ingredient count based on previous count
+    //create copy of ingredients object
+    //update count of ingredient
+    //create new price based on previous price
+    //update state
+    handleAddIngredient = type => {
+        const prevCount = this.state.ingredients[type];
+        const newCount = prevCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = newCount;
+
+        const priceAddition = INGREDIENT_PRICES[type];
+        const prevPrice = this.state.totalPrice;
+        const newPrice = prevPrice + priceAddition;
+
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
+    }
+
+    handleRemoveIngredient = type => {
+
+    }
 
     render() {
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls 
+                    ingredientAdded={this.handleAddIngredient} 
+                />
             </Fragment>
         );
     }
