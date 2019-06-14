3e96d7129db19f9bd27a7f0eea9b5ec4c38269f0
Anthony Damico <anthony.damico721@gmail.com>
2019-06-13 23:39:01 -0400

    added alternative solution for rendering ingredients

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,12 +3,27 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './Burger.module.css';
 
 const Burger = props => {
+    
+    //convert ingredients object into array of JSX elements
     let transformedIngredients = [];
     for (let ingredient in props.ingredients) {
         for (let i = 0; i < props.ingredients[ingredient]; i++) {
             transformedIngredients.push(<BurgerIngredient key={ingredient + i} type={ingredient} />)
         }
     }
+    
+    // ALTERNATIVE SOLUTION
+    // let transformedIngredients = Object.keys(props.ingredients)
+    //     .map(ingredientKey => {
+    //         return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+    //             console.log(_);
+    //             return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
+    //         });
+    //     })
+    //     .reduce((arr, el) => {
+    //         console.log(arr, el)
+    //         return arr.concat(el);
+    //     }, []);
 
     return (
         <div className={styles.burger}>
