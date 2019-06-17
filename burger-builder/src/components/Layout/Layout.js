ad7294638a7caaf73c32c885e8aab50b8637be60
Anthony Damico <anthony.damico721@gmail.com>
2019-06-17 13:13:58 -0400

    completed lecture 176

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,16 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import MobileNavigation from '../Navigation/MobileNavigation/MobileNavigation';
 import styles from './Layout.module.css';
 
-const Layout = props => (
-    <Fragment>
-        <Toolbar />
-        <MobileNavigation />
-        <main className={styles.content}>
-            {props.children}
-        </main>
-    </Fragment>
-);
+class Layout extends Component {
+    state = {
+        showMobileNav: true
+    };
+
+    handleMobileNavClosed = () => {
+        this.setState({
+            showMobileNav: false
+        });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Toolbar />
+                <MobileNavigation open={this.state.showMobileNav} closed={this.handleMobileNavClosed} />
+                <main className={styles.content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        );
+    }
+} 
 
 export default Layout;
