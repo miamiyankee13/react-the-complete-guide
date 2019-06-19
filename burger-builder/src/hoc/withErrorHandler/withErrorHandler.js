307d6ca642ae471cd908da66f91b5880d4f8e883
Anthony Damico <anthony.damico721@gmail.com>
2019-06-19 16:14:10 -0400

    completed lecture 212

--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,19 +8,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
         };
         
         componentWillMount() {
-            axios.interceptors.request.use(request => {
+            this.reqInterceptor = axios.interceptors.request.use(request => {
                 this.setState({
                     error: null
                 });
                 return request;
             });
 
-            axios.interceptors.response.use(response => response, error => {
+            this.resInterceptor = axios.interceptors.response.use(response => response, error => {
                 this.setState({
                     error
                 });
-
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         handleErrorClear = () => {
