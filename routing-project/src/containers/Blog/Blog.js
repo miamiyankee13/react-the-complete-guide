4959f47ee18d900f49c0805fe2c97a3f8993a73f
Anthony Damico <anthony.damico721@gmail.com>
2019-06-24 12:43:00 -0400

    completed lecture 240

--- routing-project/src/containers/Blog/Blog.js
+++ routing-project/src/containers/Blog/Blog.js
@@ -5,6 +5,10 @@ import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render () {
         return (
             <div className="blog">
@@ -21,7 +25,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
