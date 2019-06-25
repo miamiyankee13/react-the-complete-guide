0fe5264ba657699a13506c37f9ca8ca798bdb91e
Anthony Damico <anthony.damico721@gmail.com>
2019-06-25 17:04:08 -0400

    completed lecture 258

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styles from './Order.module.css';
 
-const Order = props => (
-    <div className={styles.order}>
-        <p>Ingredients: Lettuce (1)</p>
-        <p>Price: <strong>$5.45</strong></p>
-    </div>
-);
+const Order = props => {
+    const ingredients = [];
+    for (let ingredient in props.ingredients) {
+        ingredients.push({name: ingredient , amount: props.ingredients[ingredient]})
+    }
+    const ingredientOutput = ingredients.map(ingredient => {
+        return <span 
+                    style={{
+                        textTransform: 'capitalize', 
+                        display: 'inline-block', 
+                        margin: '0 8px',
+                        border: '1px solid #CCC',
+                        padding: '5px'
+                    }} 
+                    key={ingredient.name}
+                >
+                    {ingredient.name} ({ingredient.amount})
+                </span>
+    });
+
+    return (
+        <div className={styles.order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>${props.price.toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default Order;
